d35b67fe956a5f6e495c3d0ddb66336b95a342dd
Jeremy Shaw <jeremy@n-heptane.com>
2018-12-26 10:18:35 -0600

    0.2.7.3 - allow containers 0.6

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
-{ mkDerivation, base, containers, mtl, stdenv, text }:
+{ mkDerivation, base, containers, mtl, semigroups, stdenv, text }:
 mkDerivation {
   pname = "reform";
-  version = "0.2.7.1";
+  version = "0.2.7.2";
   src = ./.;
-  libraryHaskellDepends = [ base containers mtl text ];
+  libraryHaskellDepends = [ base containers mtl semigroups text ];
   homepage = "http://www.happstack.com/";
   description = "reform is a type-safe HTML form generation and validation library";
   license = stdenv.lib.licenses.bsd3;
