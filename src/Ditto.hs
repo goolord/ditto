1de616e212142ae69b9bf4e4a647f1f21f6f4604
goolord <zacharyachurchill@gmail.com>
2019-07-19 16:18:37 -0400

    do not reexport Data.Monoid

--- src/Ditto.hs
+++ src/Ditto.hs
@@ -1,13 +1,11 @@
 module Ditto
-    ( module Data.Monoid
-    , module Ditto.Backend
+    ( module Ditto.Backend
     , module Ditto.Core
     , module Ditto.Result
     , module Ditto.Proof
     )
     where
 
-import Data.Monoid
 import Ditto.Backend
 import Ditto.Core
 import Ditto.Result
