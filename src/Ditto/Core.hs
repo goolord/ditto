e258fa0751112bd4dd3ce4cb51dc6dd98d48c4d9
goolord <zacharyachurchill@gmail.com>
2019-07-29 11:10:10 -0400

    remove bad Monad instance

--- src/Ditto/Core.hs
+++ src/Ditto/Core.hs
@@ -177,43 +177,6 @@ instance Functor (View e) where
 -- @digestive-functors <= 0.2@.
 newtype Form m input error view a = Form {unForm :: FormState m input (View error view, m (Result error (Proved a)))}
 
--- instance (Monad m) => Functor (Form m input view error) where
---   fmap f (Form frm) =
---     Form $ do
---       (view1, mval) <- frm
---       val <- lift $ lift $ mval
---       case val of
---         (Ok (Proved posi a)) -> pure (view1, pure $ Ok (Proved posi (f a)))
---         (Error errs) -> pure (view1, pure $ Error errs)
-
--- instance (Semigroup view, Monad m) => Applicative (Form m input error view) where
---   pure a =
---     Form $ do
---       i <- getFormId
---       pure (mempty, pure $ Ok (Proved (unitRange i) a))
---   (Form frmF) <*> (Form frmA) =
---     Form $ do
---       ((view1, mfok), (view2, maok)) <-
---         bracketState $ do
---           res1 <- frmF
---           incFormId
---           res2 <- frmA
---           pure (res1, res2)
---       fok <- lift $ lift $ mfok
---       aok <- lift $ lift $ maok
---       case (fok, aok) of
---         (Error errs1, Error errs2) -> pure (view1 <> view2, pure $ Error $ errs1 ++ errs2)
---         (Error errs1, _) -> pure (view1 <> view2, pure $ Error $ errs1)
---         (_, Error errs2) -> pure (view1 <> view2, pure $ Error $ errs2)
---         (Ok (Proved (FormRange x _) f), Ok (Proved (FormRange _ y) a)) ->
---           pure
---             ( view1 <> view2
---             , pure $ Ok $ Proved
---               { pos = FormRange x y
---               , unProved = f a
---               }
---             )
-
 bracketState :: Monad m => FormState m input a -> FormState m input a
 bracketState k = do
   FormRange startF1 _ <- get
@@ -261,27 +224,6 @@ instance (Monoid view, Monad m, x ~ ()) => Applicative (Form m input error view)
               , unProved = f a
               }
             )
-
--- ???
-instance (Monoid view, Monad m, x ~ ()) => Monad (Form m input error view) where
-  formA >>= formFunction = do
-    Form $ do
-      (view0, mfok) <- unForm formA
-      fok :: Result error (Proved a) <- lift $ lift mfok
-      case fok of
-        Ok x -> do
-          (view1, mfok1) <- unForm $ formFunction $ unProved x
-          pure
-            ( view0 <> view1
-            , mfok1
-            )
-        Error _ -> do
-          (view1, mfok1) <- unForm $ formFunction =<< empty
-          pure
-            ( view0 <> view1
-            , mfok1
-            )
-          -- pure (view0, pure $ Error errs)
 
 instance (Monad m, Monoid view) => Alternative (Form m input error view) where
   empty = Form $ pure (mempty, pure $ Error mempty)
